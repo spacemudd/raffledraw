71dc557cfe73b3fae604cac4208bc1202d20feee
shafiqalshaar <shafiqalshaar@gmail.com>
2025-02-24 17:19:43 +0300

    Fix url

--- resources/views/contests/ramadan2025/create.blade.php
+++ resources/views/contests/ramadan2025/create.blade.php
@@ -213,7 +213,7 @@
             </div>
 
 
-            <div class="text-center justify-center rounded-xl my-5" style="background: url('/header.png');background-position:center;background-size:cover;height:300px;">
+            <div class="text-center justify-center rounded-xl my-5" style="background: url('/ramadan2025.png');background-position:center;background-size:cover;height:300px;">
             </div>
 
             <p class=""><a class="text-lime-800 text-xs" href="https://shop.adventurksa.com">الرئيسية</a></p>
